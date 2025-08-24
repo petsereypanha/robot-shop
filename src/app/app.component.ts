fcca10a70cc52550343cd4df22f4a7758b5c202c
petsereypanha <sereypanhapet@gmail.com>
2025-08-24 16:28:42 +0700

    add template syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CatalogComponent} from "./catalog/catalog.component";
+import {SiteHeaderComponent} from "./home/site-header/site-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent,CatalogComponent,SiteHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/catalog/product.model.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageName: string;
+  category: string;
+  discount: number;
+}
